8cd5bfbfc479624098c261610c17b1fba4d6091c
Ganesh Chandrasekaran <gchandra@gmail.com>
2025-08-18 18:35:29 -0400

    Version 0.7 - Fanout

--- src/onboard_fanout_metadata.py
+++ src/onboard_fanout_metadata.py
@@ -7,7 +7,6 @@ parser = argparse.ArgumentParser(description="Onboard Dataflowspec Parameters")
 
 parser.add_argument("--catalog_database", type=str, default="", help="Catalog database name")
 parser.add_argument("--onboarding_file_path", type=str, default="", help="Path to the onboarding file")
-parser.add_argument("--bronze_dataflowspec_table", type=str, default="bronze_dataflowspec_table", help="Bronze Dataflowspec table name")
 parser.add_argument("--silver_dataflowspec_table", type=str, default="silver_dataflowspec_table", help="Silver Dataflowspec table name")
 parser.add_argument("--env", type=str, default="dev", help="Environment (e.g., dev, prod)")
 parser.add_argument("--import_author", type=str, default="", help="Author of the import")
@@ -19,7 +18,6 @@ parser.add_argument("--uc_enabled", type=str, default="True", help="Flag to indi
 args = parser.parse_args()
 catalog_database = args.catalog_database
 onboarding_file_path = args.onboarding_file_path
-bronze_dataflowspec_table = args.bronze_dataflowspec_table
 silver_dataflowspec_table = args.silver_dataflowspec_table
 env = args.env
 import_author = args.import_author
@@ -29,7 +27,6 @@ overwrite = args.overwrite
 onboarding_params_map = {}
 onboarding_params_map["database"] = catalog_database
 onboarding_params_map["onboarding_file_path"] = onboarding_file_path
-onboarding_params_map["bronze_dataflowspec_table"] = bronze_dataflowspec_table
 onboarding_params_map["silver_dataflowspec_table"] = silver_dataflowspec_table
 onboarding_params_map["overwrite"] = overwrite
 onboarding_params_map["env"] = env
